19380f6c8922d87d66f565d0683e47c796767884
Airlangga Cahya Utama <airlangga.cahya@gmail.com>
2016-09-20 17:15:37 +0700

    Update script to use https git clone

--- src/update_script.sh
+++ src/update_script.sh
@@ -6,7 +6,7 @@ trap "rm -rf $TMP_DIR" 0 2 3 15
 CUR_DIR=$(pwd)
 
 pushd "$TMP_DIR" > /dev/null
-git clone git@github.com:durenworks/laravel-script.git .
+git clone "https://github.com/durenworks/laravel-script.git" .
 HASH=$(git rev-parse HEAD)
 
 cp -Rf src/* "$CUR_DIR"
