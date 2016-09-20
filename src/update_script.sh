e49fb2196c73402500adde6997d822505c538496
Airlangga Cahya Utama <airlangga.cahya@gmail.com>
2016-09-20 16:38:08 +0700

    Remove docker-compose file

---
+++ src/update_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TMP_DIR=`mktemp -d`
+trap "rm -rf $TMP_DIR" 0 2 3 15
+
+CUR_DIR=$(pwd)
+
+pushd "$TMP_DIR" > /dev/null
+git clone git@github.com:durenworks/laravel-script.git .
+HASH=$(git rev-parse HEAD)
+
+cp -Rf src/* "$CUR_DIR"
+echo "$HASH" > "$CUR_DIR/script.ver"
